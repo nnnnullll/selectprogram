71ba6172d9089c50806916bc256d1c8823caa82e
nnnnullll <1454672993@qq.com>
2021-02-15 16:04:30 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+}
+body{
+  margin: 0px;
 }
 </style>
--- src/components/note.vue
+++ src/components/note.vue
@@ -1,5 +1,12 @@
 <template>
-  <h1>nihao</h1>
+  <div class="notebox">
+    <div class="topshowbox">
+      <span class="top_text1">首页</span>
+      <span class="top_text2">2021-2022学年春季学期毕业设计项目管理系统</span>
+      <span class="top_text3">用户名</span>
+      <span class="top_text4">注销</span>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +16,43 @@ export default {
 </script>
 
 <style scoped>
+.notebox{
+  width: 1440px;
+  height: 768px;
+  font-family: SimHei;
+}
+.topshowbox{
+  width: 1440px;
+  height: 50px;
+  background: #2C7DC3;
+  color: #DCE7F0;
+  font-weight: 400;
+  margin-left: 0px;
 
+}
+.top_text1{
+  width: 35px;
+  height: 18px;
+  font-size: 18px;
+  margin-left: 20px;
+}
+.top_text2{
+  width: 491px;
+  height: 23px;
+  font-size: 24px;
+  font-weight: 400;
+  margin-left: 421px;
+}
+.top_text3{
+  width: 35px;
+  height: 18px;
+  font-size: 18px;
+  margin-left: 357px;
+}
+.top_text4{
+  width: 35px;
+  height: 18px;
+  font-size: 18px;
+  margin-left: 20px;
+}
 </style>
