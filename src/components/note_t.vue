3f07840b998ee0fa57a77829ce9aa86c5914024e
nnnnullll <1454672993@qq.com>
2021-02-16 17:33:35 +0800

    1

--- src/components/note_t.vue
+++ src/components/note_t.vue
@@ -1,43 +1,28 @@
 <template>
-  <div class="ktbox">
+  <div class="notebox">
     <v-top></v-top>
     <div class="mainbox">
       <v-left></v-left>
       <div class="rightbox">
-        <div class="righttitle">课题详细介绍</div>
+        <div class="righttitle">公告通知</div>
         <div class="rightline"></div>
         <!-- 要改的展示的模块就是这里 -->
         <div class="rightmainbox">
-          <div class="kt_title">
-            <div class="kt_title_text">课题介绍</div>
+          <div class="notelist_title">
+            <div class="notelist_title_text">公告列表</div>
           </div>
-          <div  class="ktrowbox">
-            <div class="ktrow_left">课题号</div>
-            <div class="ktrow_right">{{kt.kth}}</div>
+          <div class="notelist_lan">
+            <div class="notelist_lan1">发布时间</div>
+            <div class="notelist_lan2">公告对象</div>
+            <div class="notelist_lan3">公告内容</div>
           </div>
-          <div  class="ktrowbox">
-            <div class="ktrow_left">课题名称</div>
-            <div class="ktrow_right">{{kt.ktm}}</div>
-          </div>
-          <div  class="ktrowbox">
-            <div class="ktrow_left">课题类别</div>
-            <div class="ktrow_right">{{kt.ktlb}}</div>
-          </div>
-          <div  class="ktrowbox">
-            <div class="ktrow_left">课题性质</div>
-            <div class="ktrow_right">{{kt.ktxz}}</div>
-          </div>
-          <div  class="ktrowbox">
-            <div class="ktrow_left">课题来源</div>
-            <div class="ktrow_right">{{kt.ktly}}</div>
-          </div>
-          <div  class="ktrowbox">
-            <div class="ktrow_left">课题介绍</div>
-            <div class="ktrow_right">{{kt.ktjs}}</div>
-          </div>
-          <div  class="ktrowbox">
-            <div class="ktrow_left">发布时间</div>
-            <div class="ktrow_right">{{kt.fbsj}}</div>
+          <div v-for="note in notes" :key="note.key" class="notelistbox">
+            <div class="notelistrq">{{note.rq}}</div>
+            <div class="notelistdx">{{note.dx}}</div>
+            <div class="notelistnr">
+              <div class="notelistnr_bt">{{note.bt}}</div>
+              <div class="notelistnr_nr">{{note.nr}}</div>
+            </div>
           </div>
         </div>
       </div>
@@ -47,29 +32,28 @@
 
 <script>
 import vTop from '../components/topshow';
-import vLeft from '../components/leftshow_s'
+import vLeft from '../components/leftshow_t'
 const axios=require('axios');
 export default {
-  name: "ktdetail",
+  name: "note_t",
   components:{
     vTop,
     vLeft,
   },
   data(){
     return{
-      kt:[]
+      notes:[]
     }
   },
   activated:function(){
-    this.getkt();
+    this.getnote();
   },
   methods:{
-    getkt(){
-      axios.post('http://localhost:8010/gettitlebykth?kth='+1)
-      // 这里到时候缓存router跳转kth的值
+    getnote(){
+      axios.post('http://localhost:8010/getnote')
       .then((response)=>{
         console.log(response)
-        this.kt=response.data
+        this.notes=response.data
       }).catch(function (error) { // 请求失败处理
         console.log("---查询出错---！"+error);
       })
@@ -79,7 +63,7 @@ export default {
 </script>
 
 <style scoped>
-.ktbox{
+.notebox{
   width: 1440px;
   height: 768px;
   font-family: SimHei;
@@ -99,7 +83,7 @@ export default {
   flex-direction: column;
 }
 .righttitle{
-  width: 229px;
+  width: 129px;
   height: 28px;
   font-size: 30px;
   font-family: SimHei;
@@ -122,14 +106,14 @@ export default {
   height: 688px;
   overflow: auto;
 }
-.kt_title{
+.notelist_title{
   width: 1124px;
   height: 40px;
   margin-top: 20px;
   margin-left: 50px;
   background-color:#2C7DC3;
 }
-.kt_title_text{
+.notelist_title_text{
   width: 120px;
   height: 20px;
   color: white;
@@ -137,7 +121,39 @@ export default {
   padding: 10px;
   font-weight: 700;
 }
-.ktrowbox{
+.notelist_lan{
+  width: 1124px;
+  height: 40px;
+  margin-left: 50px;
+  font-size: 20px;
+  font-weight: 700;
+  display: flex;
+  flex-direction: row;
+  border-bottom: #C6CACE solid 1px;
+}
+.notelist_lan1{
+  width: 120px;
+  height: 40px;
+  text-align: center;
+  line-height: 40px;
+  border-left: #C6CACE solid 1px;
+  border-right: #C6CACE solid 1px;
+}
+.notelist_lan2{
+  width: 120px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+}
+.notelist_lan3{
+  width: 1064px;
+  height: 40px;
+  line-height: 40px;
+  text-align: center;
+  border-left: #C6CACE solid 1px;
+  border-right:#C6CACE solid 1px;
+}
+.notelistbox{
   display: flex;
   flex-direction: row;
   width: 1124px;
@@ -146,16 +162,23 @@ export default {
   font-size: 18px;
   border-bottom: #C6CACE solid 1px;
 }
-.ktrow_left{
-  width: 240px;
+.notelistrq{
+  width: 120px;
   min-height: 40px;
-  font-weight: 700;
   display: table-cell;
   text-align: center;
   line-height: 40px;
   border-left: #C6CACE solid 1px;
+  border-right: #C6CACE solid 1px;
 }
-.ktrow_right{
+.notelistdx{
+  width: 120px;
+  min-height: 40px;
+  line-height: 40px;
+  text-align: center;
+  vertical-align: middle;
+}
+.notelistnr{
   width: 1064px;
   min-height: 40px;
   text-align:left;
@@ -164,5 +187,12 @@ export default {
   border-right:#C6CACE solid 1px;
   display: flex;
   flex-direction: column;
+}
+.notelistnr_bt{
+  font-weight: 700;
+   line-height: 40px;
+}
+.notelistnr_nr{
+  line-height: 30px;
 }
 </style>
